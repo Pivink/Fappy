3f5a078051fb1650a62c0b89852def883b5fd8a5
Pivink <161929786+Pivink@users.noreply.github.com>
2025-08-28 05:10:46 +0530

    api set

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:4000/api/f1';
+const API_URL = 'https://fappy.onrender.com/api/f1';
 
 const api = axios.create({
   baseURL: API_URL,
